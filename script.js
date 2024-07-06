b2c4a72193f8758a39d76fee122b1a7e3f639598
Josh Baron <50253167+xriiitox@users.noreply.github.com>
2024-07-06 02:47:55 +0000

    js and close button and other stuff

---
+++ script.js
@@ -0,0 +1,65 @@
+setInterval( () => { 
+  document.querySelector("#time").innerHTML = new Date().toLocaleString() 
+}, 1000);
+
+function dragElement(element) {
+  var initialX, initialY, x, y;
+
+  if (document.getElementById(element.id + "header")) {
+    document.getElementById(element.id + "header").onmousedown = startDragging; 
+  } else {
+    element.onmousedown = startDragging;
+  }
+
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    initialX = e.clientX;
+    initialY = e.clientY;
+
+    document.onmouseup = stopDragging;
+    document.onmousemove = dragElement;
+  }
+
+  function dragElement(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+dragElement(document.getElementById("welcome"))
+
+var welcomeScreen = document.querySelector("#welcome");
+
+function closeWindow(element) {
+  element.style.display = "none";
+}
+
+function openWindow(element) {
+  element.style.display = "flex";
+}
+
+var welcomeScreenClose = document.querySelector("#welcome-cross");
+var welcomeScreenOpen = document.querySelector("#welcomeopen");
+
+welcomeScreenClose.onclick = () => {
+  closeWindow(welcomeScreen);
+}
+
+welcomeScreenOpen.onclick = () => {
+  openWindow(welcomeScreen);
+}
